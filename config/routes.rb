fcf6ac4a1dd233728bd40212d0085d9855fd8024
malchak <michaelmalchak@gmail.com>
2013-10-17 12:15:54 -0700

    Setup routes in config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 EmberApp::Application.routes.draw do
+  class FormatTest
+    attr_accessor :mime_type
+
+    def initialze(format)
+      @mime_type = Mime::Type.lookup_by_extension(format)
+    end
+
+    def matches?(request)
+      request.format == mime_type
+    end
+  end
+
+  resources :users, :execpt => :edit, :constraints => FormatTest.new(:json)
+  get '*foo', :to => 'ember#index', :constraints => FormatTest.new(:html)
+  get '/', :to => 'ember#index', :constraints => FormatTest.new(:html)
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
